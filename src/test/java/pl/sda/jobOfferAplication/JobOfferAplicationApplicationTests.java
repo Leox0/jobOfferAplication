08e3a1b2002f860988885284568e9cecb037d6dd
PCz <mail.00.kontakt@gmail.com>
2020-08-01 11:59:34 +0200

    initial commit

---
+++ src/test/java/pl/sda/jobOfferAplication/JobOfferAplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.jobOfferAplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobOfferAplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
