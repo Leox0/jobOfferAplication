03436f8f3b4aa4ae49d98f7db70daafe7f5cf93a
borowskiadam92 <64302729+borowskiadam92@users.noreply.github.com>
2020-08-22 22:04:16 +0200

    Test for option of deleting  user, happy and non happy path (#9)
    * Test for option of deleting of user, happy and non happy path
    * repair method deleteUserById, refactor code in UserServiceImpl, repair delete tests
    
    merge PC

--- src/test/java/pl/sda/jobOfferAplication/user/service/UserServiceTest.java
+++ src/test/java/pl/sda/jobOfferAplication/user/service/UserServiceTest.java
@@ -1,6 +1,5 @@
 package pl.sda.jobOfferAplication.user.service;
 
-import net.bytebuddy.implementation.bytecode.Throw;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
@@ -9,7 +8,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import pl.sda.jobOfferAplication.user.entity.UserEntity;
 import pl.sda.jobOfferAplication.user.exception.UserException;
 import pl.sda.jobOfferAplication.user.model.UserInput;
-import pl.sda.jobOfferAplication.user.model.UserOutput;
 import pl.sda.jobOfferAplication.user.repository.UserRepository;
 
 
@@ -29,6 +27,7 @@ class UserServiceTest {
 
     @AfterEach
     void tearDown() {
+
         userRepository.deleteAll();
     }
 
@@ -68,7 +67,7 @@ class UserServiceTest {
     }
 
     @Test
-    public void shouldThrowExceptionWhenLoginIsTooShort() throws UserException {
+    public void shouldThrowExceptionWhenLoginIsTooShort() {
         //given
         UserInput userInput = new UserInput("Tomek", "Tome2", "Tomek231@");
 
@@ -85,7 +84,7 @@ class UserServiceTest {
 
 
     @Test
-    public void shouldThrowExceptionWhenUserPasswordIsIncorrect() throws UserException {
+    public void shouldThrowExceptionWhenUserPasswordIsIncorrect(){
         //given
         UserInput userInput = new UserInput("Tomek", "Tomek12345", "Tomek231");
 
@@ -98,5 +97,31 @@ class UserServiceTest {
 
         UserException userException = assertThrows(UserException.class, executable);
         assertEquals(USER_PASSWORD_IS_INCORRECT, userException.getMessage());
+    }
+
+    @Test
+    public void withPossibilityToDeleteUser() throws UserException {
+        //given
+        UserInput userInput = new UserInput("Tomek", "Tomek12345", "Tomek231@");
+        userService.createUser(userInput);
+
+        //when
+        userService.deleteUserById(1L);
+
+        //then
+        List<UserEntity> allUsers = userRepository.findAll();
+        assertTrue(allUsers.size() == 0);
+    }
+
+    @Test
+    public void shouldThrowExceptionDeletingUserWhenThereIsNoUser() {
+        //given
+
+        //when
+        Executable executable = () -> userService.deleteUserById(1L);
+
+        //then
+        UserException userException = assertThrows(UserException.class, executable);
+        assertEquals(NO_USER_FOUND_FOR_GIVEN_ID, userException.getMessage());
     }
 }
