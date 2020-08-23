47a32c685ed3c744251c7ddecb90ec87501f46c9
PCz <mail.00.kontakt@gmail.com>
2020-08-23 15:03:03 +0200

    in progress

--- src/main/java/pl/sda/jobOfferAplication/jobOffer/controller/JobOfferController.java
+++ src/main/java/pl/sda/jobOfferAplication/jobOffer/controller/JobOfferController.java
@@ -1,4 +1,11 @@
 package pl.sda.jobOfferAplication.jobOffer.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(JobOfferController.JOB_OFFER_MAPPING)
 public class JobOfferController {
+    public static final String JOB_OFFER_MAPPING = "/jobOffer";
+
 }
--- src/main/java/pl/sda/jobOfferAplication/jobOffer/controller/JobOfferControllerAdvice.java
+++ src/main/java/pl/sda/jobOfferAplication/jobOffer/controller/JobOfferControllerAdvice.java
@@ -1,4 +1,17 @@
 package pl.sda.jobOfferAplication.jobOffer.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import pl.sda.jobOfferAplication.jobOffer.exception.JobOfferException;
+
+@ControllerAdvice
 public class JobOfferControllerAdvice {
+/*    @ExceptionHandler(JobOfferException.class)
+    public ResponseEntity<String> handUserException(JobOfferException e) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(e.getMessage());
+    }*/
 }
